c5d5b9fddabf213ec6e069c45065e5e81f170567
ryota-komatsu <komatsu.r.ab@gmail.com>
2025-03-20 19:57:37 +0900

    add server option to data downloading

--- scripts/download_librispeech.sh
+++ scripts/download_librispeech.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
 dataset_root=${1:-data}
+server=${2:-default}
 
-wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/train-clean-100.tar.gz
-wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/train-clean-360.tar.gz
-wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/train-other-500.tar.gz
-wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/dev-clean.tar.gz
-wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/dev-other.tar.gz
-wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/test-clean.tar.gz
-wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/test-other.tar.gz
+if [ ${server} = "default" ]; then
+    # default server
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/train-clean-100.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/train-clean-360.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/train-other-500.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/dev-clean.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/dev-other.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/test-clean.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://www.openslr.org/resources/12/test-other.tar.gz
+elif [ ${server} = "cn" ]; then
+    # CN mirror server
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://openslr.magicdatatech.com/resources/12/train-clean-100.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://openslr.magicdatatech.com/resources/12/train-clean-360.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://openslr.magicdatatech.com/resources/12/train-other-500.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://openslr.magicdatatech.com/resources/12/dev-clean.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://openslr.magicdatatech.com/resources/12/dev-other.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://openslr.magicdatatech.com/resources/12/test-clean.tar.gz
+    wget -t 0 -c -P ${dataset_root}/LibriSpeech https://openslr.magicdatatech.com/resources/12/test-other.tar.gz
+fi
 
 tar zxvf ${dataset_root}/LibriSpeech/train-clean-100.tar.gz -C ${dataset_root}
 tar zxvf ${dataset_root}/LibriSpeech/train-clean-360.tar.gz -C ${dataset_root}
--- scripts/download_libritts.sh
+++ scripts/download_libritts.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
 dataset_root=${1:-data}
+server=${2:-default}
 
-wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/train_clean_100.tar.gz
-wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/train_clean_360.tar.gz
-wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/train_other_500.tar.gz
-wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/dev_clean.tar.gz
-wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/dev_other.tar.gz
-wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/test_clean.tar.gz
-wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/test_other.tar.gz
+if [ ${server} = "default" ]; then
+    # default server
+    wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/train_clean_100.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/train_clean_360.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/train_other_500.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/dev_clean.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/dev_other.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/test_clean.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://www.openslr.org/resources/141/test_other.tar.gz
+elif [ ${server} = "cn" ]; then
+    # CN mirror server
+    wget -t 0 -c -P ${dataset_root} https://openslr.magicdatatech.com/resources/141/train_clean_100.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://openslr.magicdatatech.com/resources/141/train_clean_360.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://openslr.magicdatatech.com/resources/141/train_other_500.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://openslr.magicdatatech.com/resources/141/dev_clean.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://openslr.magicdatatech.com/resources/141/dev_other.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://openslr.magicdatatech.com/resources/141/test_clean.tar.gz
+    wget -t 0 -c -P ${dataset_root} https://openslr.magicdatatech.com/resources/141/test_other.tar.gz
+fi
 
 tar zxvf ${dataset_root}/train_clean_100.tar.gz -C ${dataset_root}
 tar zxvf ${dataset_root}/train_clean_360.tar.gz -C ${dataset_root}
